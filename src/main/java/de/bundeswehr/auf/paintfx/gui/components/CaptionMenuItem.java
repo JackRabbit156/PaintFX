c77fcfd40e32c87a6f45659ae5737f43f660d7a5
Frank Rieger <frankrieger@auf.bundeswehr.de>
2024-11-20 15:04:35 +0100

    Fehler behoben

--- src/main/java/de/bundeswehr/auf/paintfx/gui/components/CaptionMenuItem.java
+++ src/main/java/de/bundeswehr/auf/paintfx/gui/components/CaptionMenuItem.java
@@ -3,11 +3,6 @@ package de.bundeswehr.auf.paintfx.gui.components;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 
-/**
- * Known bugs:
- * <li>gets selected background, when separator next to this and mouse is over separator</li>
- * <li>gets selected background, when first item in {@link javafx.scene.control.MenuButton} and mouse moves back to the {@link javafx.scene.control.MenuButton}</li>
- */
 public class CaptionMenuItem extends CustomMenuItem {
 
     private static final String DEFAULT_STYLE_CLASS = "caption-menu-item";
